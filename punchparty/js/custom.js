3c781777bacb64d95aac5cbe604d3398fd71abd5
Chawne Harris <chawneangelo@gmail.com>
2022-02-09 19:15:15 -0600

    Adding Remaining Site Files and Web Files

---
+++ punchparty/js/custom.js
@@ -0,0 +1,13 @@
+
+})(jQuery);
+console.log('it works')
+$(document).ready(function(){
+    $('.submit').click(function(event){
+    
+        console.log('Clicked Button')
+        var email= $('.email').val()
+
+        if(.name.isEmpty)
+        event.preventDefault()
+    })
+})
